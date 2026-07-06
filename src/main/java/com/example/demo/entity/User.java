216a690162491590d723f29bbd5547db80d7c994
Dannoruwa-Anush <dannoruwalakshitha@gmail.com>
2026-07-07 04:33:01 +0530

    lombok is added

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,14 +1,26 @@
 package com.example.demo.entity;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "users")
+@NoArgsConstructor // needed for JPA
+@AllArgsConstructor
+@Data // getters & setters
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,44 +30,27 @@ public class User {
     private String name;
 
     @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
     private String password;
 
-    public User() {
-        
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+    
+    @PrePersist // is called before the entity is inserted
+    public void onPrePersist() {
+        ZonedDateTime zonedNowUtc = ZonedDateTime.now(ZoneId.of("UTC")); // Convert to UTC time zone
+        this.createdAt = zonedNowUtc.toLocalDateTime(); // Convert to LocalDateTime for storage
+        this.updatedAt = zonedNowUtc.toLocalDateTime(); // Convert to LocalDateTime for storage
     }
 
-    public User(String name, String password) {
-        this.name = name;
-        this.password = password;
-    }
-
-    public User(long id, String name, String password) {
-        setId(id);
-        setName(name);
-        setPassword(password);
-    }
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
+    @PreUpdate // is called before the entity is updated
+    public void onPreUpdate() {
+        ZonedDateTime zonedNowUtc = ZonedDateTime.now(ZoneId.of("UTC"));// Convert to UTC time zone
+        this.updatedAt = zonedNowUtc.toLocalDateTime(); // Convert to LocalDateTime for storage
     }
 }
