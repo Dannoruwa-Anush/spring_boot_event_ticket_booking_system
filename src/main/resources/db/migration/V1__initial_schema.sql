a5ab9cfe35f30f63f8825fb3732802ddb0f75962
Dannoruwa-Anush <dannoruwalakshitha@gmail.com>
2026-07-12 11:41:31 +0530

    Schema is corrected

--- src/main/resources/db/migration/V1__initial_schema.sql
+++ src/main/resources/db/migration/V1__initial_schema.sql
@@ -6,15 +6,27 @@
 
 CREATE TABLE roles (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(50) NOT NULL
-);
+    name VARCHAR(50) NOT NULL,
 
+    created_at DATETIME NOT NULL,
+    updated_at DATETIME NOT NULL,
+    created_by VARCHAR(255),
+    updated_by VARCHAR(255)
+);
 
 CREATE TABLE users (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(100) NOT NULL,
+
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
 
     role_id BIGINT,
+
+    created_at DATETIME NOT NULL,
+    updated_at DATETIME NOT NULL,
+    created_by VARCHAR(255),
+    updated_by VARCHAR(255),
 
     CONSTRAINT fk_user_role
         FOREIGN KEY (role_id)
