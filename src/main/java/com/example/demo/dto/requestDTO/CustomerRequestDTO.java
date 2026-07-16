a7fa95addd37dd9db7110ef48bf091ee79eeb1d5
Dannoruwa-Anush <dannoruwalakshitha@gmail.com>
2026-07-16 08:22:51 +0530

    dto are added

---
+++ src/main/java/com/example/demo/dto/requestDTO/CustomerRequestDTO.java
@@ -0,0 +1,16 @@
+package com.example.demo.dto.requestDTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class CustomerRequestDTO {
+    private long id;
+    private String address;
+    private String date_of_birth;
+    private String phoneNo;
+    private UserRequestDTO user;
+}
---
+++ src/main/java/com/example/demo/dto/responseDTO/PositionResponseDTO.java
@@ -0,0 +1,5 @@
+package com.example.demo.dto.responseDTO;
+
+public class PositionResponseDTO {
+    
+}
