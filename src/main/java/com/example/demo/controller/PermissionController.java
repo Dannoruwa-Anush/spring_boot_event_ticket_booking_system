cc968c89d924cbb04ccdf706ecd371d536708ae9
Dannoruwa-Anush <dannoruwalakshitha@gmail.com>
2026-09-18 07:02:48 +0530

    unnecessary apis are removed

--- src/main/java/com/example/demo/controller/PermissionController.java
+++ src/main/java/com/example/demo/controller/PermissionController.java
@@ -3,16 +3,11 @@ package com.example.demo.controller;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.demo.dto.requestDTO.PermissionRequestDTO;
 import com.example.demo.dto.responseDTO.PermissionResponseDTO;
 import com.example.demo.dto.responseDTO.common.PageResponseDTO;
 import com.example.demo.service.PermissionService;
@@ -26,12 +21,6 @@ public class PermissionController {
         this.service = service;
     }
 
-    @PostMapping
-    public ResponseEntity<PermissionResponseDTO> createPermission(@RequestBody PermissionRequestDTO dto) {
-        PermissionResponseDTO response = service.createPermission(dto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(response);
-    }
-
     @GetMapping
     public ResponseEntity<PageResponseDTO<PermissionResponseDTO>> getAllPermissions(Pageable pageable) {
         PageResponseDTO<PermissionResponseDTO> response = service.getAllPermissions(pageable);
@@ -42,17 +31,5 @@ public class PermissionController {
     public ResponseEntity<PermissionResponseDTO> getPermissionById(@PathVariable Long id) {
         PermissionResponseDTO response = service.getPermissionById(id);
         return ResponseEntity.status(HttpStatus.OK).body(response);
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<PermissionResponseDTO> updatePermission(@PathVariable Long id, @RequestBody PermissionRequestDTO dto) {
-        PermissionResponseDTO response = service.updatePermission(id, dto);
-        return ResponseEntity.status(HttpStatus.OK).body(response);
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deletePosition(@PathVariable Long id) {
-        service.deletePermission(id);
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
 }
